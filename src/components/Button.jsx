db9ccd9bffe8d4a2daccae22e49ef3060a854ec4
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-20 12:14:05 -0500

    state and events

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const Button = props => {
-    const { text } = props; // destructuring
+class Button extends Component {
+  state = {
+    count: 0
+  };
+
+  handleClick = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  render() {
+    const { count } = this.state;
     return (
-        <div>
-            <button type="button">{props.text}</button>
-            <button type="button">{text}</button>
-        </div>
-    )
+      <div>
+        <h1>Manzanas: {count}</h1>
+        <button onClick={this.handleClick} type="button">
+          Agregar
+        </button>
+      </div>
+    );
+  }
 }
+const ButtonStateLess = props => {
+  const { text } = props; // destructuring
+  return (
+    <div>
+      <button type="button">{props.text}</button>
+      <button type="button">{text}</button>
+    </div>
+  );
+};
 export default Button;

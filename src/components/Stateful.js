bdca753963c48dca621b208b30288f74bce2733c
Carlos Jaramillo <Carlos.Jaramillo@blackboard.com>
2020-03-20 08:28:07 -0500

    First components types

---
+++ src/components/Stateful.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Stateful extends Component {
+  // Have lifecycle, events and state
+  constructor(props) {
+    super(props);
+    this.state = {
+      hello: "Hola mundo"
+    };
+  }
+  render() {
+    return <h1>{this.state.hello}</h1>;
+  }
+}
+
+export default Stateful;
